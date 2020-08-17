6aa868cf36e47a0f11a89cf249ebbb2a518a163b
Juan Ramirez Jardua <lch_jramirez@hotmail.com>
2020-08-16 20:13:16 -0400

    ROBOT_hardware_interface

---
+++ ROBOT_hardware_interface/src/ROBOT_hardware_interface_node.cpp
@@ -0,0 +1,15 @@
+#include <ROBOT_hardware_interface/ROBOT_hardware_interface.h>
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "ROBOT_hardware_interface");
+    ros::CallbackQueue ros_queue;
+
+    ros::NodeHandle nh;
+    nh.setCallbackQueue(&ros_queue);
+    ROBOT_hardware_interface::ROBOTHardwareInterface rhi(nh);
+
+    ros::MultiThreadedSpinner spinner(0);
+    spinner.spin(&ros_queue);
+    return 0;
+}
+
